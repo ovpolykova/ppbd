79cd8c340c08494b13d58e41e33310eb9477061f
nikita42423 <nikita42423@bk.ru>
2023-03-17 15:45:51 +0300

    Большое изменение (весь сайт)

---
+++ application/controllers/Pages.php
@@ -0,0 +1,19 @@
+<?php
+	class Pages extends CI_Controller {
+
+	public function view($page = 'home')
+	{
+        if ( ! file_exists(APPPATH.'/views/pages/'.$page.'.php'))
+        {
+            // Упс, у нас нет такой страницы!
+            show_404();
+        }
+
+        $data['title'] = ucfirst($page);
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/navbar', $data);
+        $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/footer', $data);
+	}
+}
