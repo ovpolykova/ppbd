05ec33d7d2713ffc4647d45915db8bbf2062f888
Xitonosjori <nikoprucho@mail.ru>
2023-03-17 13:38:53 +0300

    footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,7 @@
 <footer>
 <div class="footer">
 <p><?php echo '&copy 2023'; ?>
-XXXXXXXXXXX, студент(кa) специальности 09.02.03, 4 курс группа -XXXXXX </p>
+XXXXXXXXXX, студент(кa) специальности 09.02.03, 4 курс группа -XXXXXX </p>
 </div>
 </footer>
 
