fd90f0da75314e7a4509a1358d02d9c6964dc475
ilyakh_usa@mail.ru <ilyakh_usa@mail.ru>
2023-03-17 11:09:20 +0300

    изменилось

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://ppbd/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'fffff';
+$config['index_page'] = 'index21.php';
 
 /*
 |--------------------------------------------------------------------------
