72c699d625f2d87b35f9f2fb3f5ea0df0c427780
ilyakh_usa@mail.ru <ilyakh_usa@mail.ru>
2023-03-18 11:40:45 +0300

    в

--- application/views/templates/navbar_oper.php
+++ application/views/templates/navbar_oper.php
@@ -2,7 +2,7 @@
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <a href="home.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
             <img class="bi me-2" src="<?=asset_url()?>/img/logo1.png" alt="" width="100" height="80">
-            <p><b>Добро пожаловать, Менеджер! ФИО</b></p>
+            <p><b>Добро пожаловать, Оператор! ФИО</b></p>
         </a>
 
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
