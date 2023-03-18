1fb4f5b70c9dcfae4990b13a189afc604d8a0ddc
ilyakh_usa@mail.ru <ilyakh_usa@mail.ru>
2023-03-18 11:14:31 +0300

    й

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -5,16 +5,9 @@
         </a>
 
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
-            <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+            <li><a href="index.php" class="nav-link px-2 link-secondary">АВТОРИЗАЦИЯ</a></li>
         </ul>
 
-        <div class="col-md-3 text-end">
-            <button type="button" class="btn btn-outline-primary me-2">Авторизация</button>
-            <button type="button" class="btn btn-primary">Регистрация</button>
-        </div>
+       
     </header>
 </div>
