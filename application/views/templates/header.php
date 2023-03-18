ae612b83b0b5f57a9d5b272e8557330eb1befe47
ilyakh_usa@mail.ru <ilyakh_usa@mail.ru>
2023-03-18 11:01:10 +0300

    изменил

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <head>
-	<title> Курсовой проект </title>
+	<title>Официальный сайт "Заказ" | компания "DEAF"</title>
 	<link rel="stylesheet" href="<?=asset_url()?>/css/bootstrap.css">
+	<script src="<?=asset_url()?>/js/bootstrap.js"></script>
+	<script src="<?=asset_url()?>/js/jquery-3.6.4.min.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">                                                   
 </head>
 <body>
