3edcc1d0dab11976b3a0ddef274f118cf9361183
nikita42423 <nikita42423@bk.ru>
2023-03-20 05:02:51 +0300

    Большое изменение (2 часть)

--- application/views/templates/navbar_operator.php
+++ application/views/templates/navbar_operator.php
@@ -1,6 +1,6 @@
 <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <a href="#" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
             <img class="bi me-2" src="<?=asset_url()?>/img/bootstrap-logo.svg" alt="" width="40" height="32">
         </a>
 
