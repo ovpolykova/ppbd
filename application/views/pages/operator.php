53d7ba8de4ba84acad2255a76ca5641bfd132e1f
nikita42423 <nikita42423@bk.ru>
2023-03-20 15:15:00 +0300

    С нуля

--- application/views/pages/operator.php
+++
@@ -1,7 +0,0 @@
-<div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="<?=asset_url()?>/img/bootstrap-logo.svg" alt="" width="72" height="57">
-        <h1 class="display-5 fw-bold">Добро пожаловать, оператор <?=$session['fio']?>!</h1>
-    <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4">Приступайте к работе!</p>
-    </div>
-</div>
