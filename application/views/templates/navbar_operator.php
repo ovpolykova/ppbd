f45f7f8488dce7e63f4510618131bf939893a714
ilyakh_usa@mail.ru <ilyakh_usa@mail.ru>
2023-03-20 15:46:05 +0300

    шапка футер и меню изменилось

---
+++ application/views/templates/navbar_operator.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <a href="home.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <img class="bi me-2" src="<?=asset_url()?>/img/logo1.png" alt="" width="100" height="80">
+            <p><b>Добро пожаловать, Оператор! ФИО</b></p>
+        </a>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="contractor_browse.php" class="nav-link px-2 link-secondary">Справочник контрагентов</a></li>
+            <li><a href="order.php" class="nav-link px-2 link-dark">Заказ</a></li>
+            
+        </ul>
+
+        <div class="col-md-3 text-end">
+        <a href="home.php" class="nav-link px-2 link-dark"> <button type="button" class="btn btn-outline-primary me-2">ВЫЙТИ</button></a> 
+        </div>
+    </header>
+</div>
