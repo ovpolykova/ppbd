f9ce7ca7cfda06ddfa5063fb658fdda9a4861def
nikita42423 <nikita42423@bk.ru>
2023-03-21 14:35:39 +0300

    фф

---
+++ application/models/Report.php
@@ -0,0 +1,31 @@
+<?php
+class Report extends CI_Model {
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    //Харламов
+    public function sel_contractor_delivery()
+    {
+        
+    }
+
+    //Пручковский
+    public function sel_rep_order_contractor()
+    {
+        
+    }
+
+    public function sel_rep_order_product()
+    {
+        
+    }
+
+    //Кузнецов
+    public function sel_rep_order_fall()
+    {
+        
+    }
+}
