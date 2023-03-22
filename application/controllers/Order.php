8f759238aad5563e6b24425c5e5aac0c6eaf8ed2
ilyakh_usa@mail.ru <ilyakh_usa@mail.ru>
2023-03-22 15:08:06 +0300

    bpvt

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -11,6 +11,25 @@
         $this->load->view('templates/footer');
 	}
 
+    public function upd_order()
+	{
+        $this->load->model('order_m');
+        $data['browesorder'] = $this->order_m->upd_order_status();
+        $this->load->view('templates/header');
+        $this->load->view('templates/navbar_operator');
+        $this->load->view('pages/order_delivery', $data);
+        $this->load->view('templates/footer');
+	}
+
+    public function rep_delivery()
+	{
+        $this->load->model('order_m');
+        $data['browesorder'] = $this->order_m->sel_order_delivery();
+        $this->load->view('templates/header');
+        $this->load->view('templates/navbar_operator');
+        $this->load->view('pages/order_delivery', $data);
+        $this->load->view('templates/footer');
+	}
 
 
 }
