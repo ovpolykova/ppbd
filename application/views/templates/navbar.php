d9721ed577c38b2187b96e56e2460787081af934
nikita42423 <nikita42423@bk.ru>
2023-03-23 01:38:10 +0300

    a

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,7 +1,7 @@
 <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-        <a href="<?=base_url('home/index')?>" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-            <img class="bi me-2" src="<?=asset_url()?>/img/logo1.png" alt="" width="100" height="80">
+        <a href="<?=base_url()?>" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <img class="bi me-2" src="<?=asset_url()?>/img/logo.jpg" alt="" width="30%" height="30%">
         </a>
 
         <div class="col-md-3 text-end">
--- application/views/templates/navbar_operator.php
+++ application/views/templates/navbar_operator.php
@@ -1,7 +1,7 @@
 <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <a href="<?=base_url('home/index')?>" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-            <img class="bi me-2" src="<?=asset_url()?>/img/logo1.png" alt="" width="100" height="80">
+            <img class="bi me-2" src="<?=asset_url()?>/img/logo.jpg" alt="" width="30%" height="30%">
             <p><b>Добро пожаловать, Оператор! ФИО</b></p>
         </a>
 
