da690dff438dfb1e6bfcb2b815bd664e5b98dbb9
nikita42423 <nikita42423@bk.ru>
2023-04-03 14:36:15 +0300

    ф

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -54,8 +54,8 @@
     //Просмотр заказов, не выполненных в срок|Кузнецов
     public function browse_order_fall()
     {
-        //Сессия
-		$data['session'] = $this->session->userdata('login_session');
+        $this->load->model('report_m');
+        $this->load->model('report_m');
 
         $date1 = "2000-01-01";
         $date2 = "2100-01-01";
@@ -68,8 +68,10 @@
             $date2 = $this->input->post('date2');
         }
 
-        $this->load->model('report_m');
         $data['order_fall'] = $this->report_m->sel_rep_order_fall($date1, $date2);
+
+        //Сессия
+		$data['session'] = $this->session->userdata('login_session');
 
         $this->load->view('templates/header');
         $this->load->view('templates/navbar_admin', $data);
