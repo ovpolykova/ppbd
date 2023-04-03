d9acd5b1761ba63d178316a5005d716b7aa12e68
FreedomAndrey <andrey.volobuev.20@mail.ru>
2023-04-03 16:33:34 +0300

    ф

--- application/views/pages/home.php
+++
@@ -1,8 +0,0 @@
-<!-- Главная страница|Кузнецов -->
-<div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="<?=asset_url()?>/img/logo.jpg" alt="" width="10%" height="10%">
-    <h1 class="display-5 fw-bold">Добро пожаловать, гость!</h1>
-    <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4">Авторизуйтесь!</p>
-    </div>
-</div>
--- application/views/templates/footer.php
+++
@@ -1,5 +0,0 @@
-    <footer class="my-5 pt-5 text-muted text-center text-small">
-        <p class="mb-1">&copy; 2023 Компания | DEAF</p>
-    </footer>
-    <script src="<?=asset_url()?>/js/bootstrap.bundle.js"></script>
-</body>
